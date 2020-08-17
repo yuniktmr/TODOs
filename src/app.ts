76a97f267ac6b6ad44f7f65708412e36a80ca87d
Yunik Tamrakar <yuniktmr@gmail.com>
2020-08-17 11:49:00 -0600

    finished

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import { ProjectInput } from './components/project-input';
+import { ProjectList } from './components/project-list'; 
+
+
+    export const prjInput = new ProjectInput();
+    export const activePrjList = new ProjectList('active');
+    export const finishedPrjList = new ProjectList('finished');
+console.log("hi");
+
